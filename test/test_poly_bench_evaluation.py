4ed124de3e2dcb201030f3cc98d75bb9d52fde71
Shihab Rashid <mshihabr@amazon.com>
2025-04-03 20:43:04 +0000

    prepare the submission branch

--- test/test_poly_bench_evaluation.py
+++
@@ -1,8 +0,0 @@
-"""Tests for PolyBench-Evaluation module."""
-
-import pytest  # noqa: F401
-
-
-def test_poly_bench_evaluation_importable():
-    """Test poly_bench_evaluation is importable."""
-    import poly_bench_evaluation  # noqa: F401
